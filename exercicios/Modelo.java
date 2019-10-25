8c187af366ef408f440ff29de530043d1b2b1a8c
joaomcode <joaomarcelo.co@hotmail.com>
2019-10-25 19:15:52 -0300

    Adicionando o arquivo Modelo.java ao diretório exercicios

---
+++ exercicios/Modelo.java
@@ -0,0 +1,18 @@
+/*
+ *Class: Class modelo
+ *
+ *Versão: 1.0
+ *
+ *Data: ------
+ * 
+ *Autor: joaomcode
+ *
+ */
+
+public class Modelo {
+	public static void main(String[] args){
+		System.out.println("Modelo!");
+	}
+}
+
+
