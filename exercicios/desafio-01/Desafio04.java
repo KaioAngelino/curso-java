5215f2ffac6ac9247041e82949360705f3f93302
joaomcode <joaomarcelo.co@hotmail.com>
2019-11-04 18:48:22 -0300

    Resolução da questão proposta

--- exercicios/desafio-01/Desafio04.java
+++ exercicios/desafio-01/Desafio04.java
@@ -29,7 +29,41 @@ public class Desafio04 {
 
 		// Intância um objeto leitor
 		Scanner leitor = new Scanner(System.in);
-		System.out.printf("O valor final é %.2f " , valorFinal);
+
+		// Solicita ao usuário valores do código e a quantidade a levar
+		System.out.print("Digite o código do produto: ");
+		int codigo = leitor.nextInt();
+		System.out.print("Qual a quantidade o cliente quer levar: ");
+		int quantidade = leitor.nextInt();
+
+		// Instancia a variável valor e a zera
+		double valor = 0;
+
+		// Realiza os cálculos do valor total a se pagar
+		// de acordo com o código digitado
+		switch (codigo) {
+			case 100:
+				valor = quantidade * 1.7;
+				break;
+			case 101:
+				valor = quantidade * 2.3;
+				break;
+			case 102:
+				valor = quantidade * 2.6;
+				break;
+			case 103:
+				valor = quantidade * 2.4;
+				break;
+			case 104:
+				valor = quantidade * 2.5;
+				break;
+			case 105:
+				valor = quantidade * 1;
+				break;
+		}
+
+		// Apresenta o valor final a ser pago
+		System.out.printf("O valor total é: R$%.2f",valor);
 		System.out.println("");
 	}
 }
