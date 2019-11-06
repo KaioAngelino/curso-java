5172f9c96dba7245b13602ed0508fb4602c092ed
joaomcode <joaomarcelo.co@hotmail.com>
2019-11-06 16:01:33 -0300

    Atualização

--- exercicios/aula010/ListaDeVetores.java
+++ exercicios/aula010/ListaDeVetores.java
@@ -15,16 +15,44 @@ import java.util.ArrayList;
 public class ListaDeVetores {
 	public static void main(String[] args){
 		
+		// Instanciamento da lista
 		ArrayList<String> cadastro = new ArrayList<>();
 		
+		// Adiciona o valor dentro da lista
 		cadastro.add("João");
-		cadastro.remove(0);
-		cadastro.add("Zé");
-		cadastro.add("Capetinha");
-		cadastro.add("1 2 3");
-		cadastro.add("até");
 		
+		// Remove o valor da posição indicada
+		cadastro.remove(0);
+		
+		cadastro.add("Zé");
+		cadastro.add("Luiz");
+		
+		// Adiciona o valor dentro da lista na posição indicada
+		cadastro.add(1,"Paulo");
+		cadastro.add(0,"Karine");
+		
+		// Mostra na tela a lista
 		System.out.println(cadastro);
+		
+		// Limpa a lista
+		cadastro.clear();
+		
+		
+		cadastro.add("Luiz");
+		cadastro.add("Paulo");
+		
+		// Mostra na tela a lista
+		System.out.println(cadastro);
+		
+		// Faz uma busca na lista o nome digitado (Luiz)
+		System.out.println(cadastro.contains("Luiz"));		
+		System.out.println(cadastro.contains("João"));		
+		// Retorna a posição na lista o nome digitado (Luiz)
+		System.out.println(cadastro.indexOf("Luiz"));
+		// Mostra na tela o tamanho da lista
+		System.out.println(cadastro.size());		
+		
+		
 	}
 }
 
