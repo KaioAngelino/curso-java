6d0a52fe4349a0c64cbc4ef8c729ad8ebb66024b
joaomcode <joaomarcelo.co@hotmail.com>
2019-11-22 16:50:17 -0300

    Demonstração dos novos formatos de apresentação de data Java8 e superior

--- poo/src/joaomcode/aula022/data/Data.java
+++ poo/src/joaomcode/aula022/data/Data.java
@@ -1,5 +1,6 @@
 package joaomcode.aula022.data;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,6 +12,14 @@ public class Data {
 
 		// Date data = new Date();
 		Date data = c.getTime();
+
+		DateFormat hora = DateFormat.getTimeInstance();
+
+		System.out.println("hora: " + hora.format(data));
+		
+		DateFormat data2 = DateFormat.getDateInstance();
+		
+		System.out.println("Data: "+data2.format(data));
 
 		System.out.println(data);
 
